7ed4ea25faf24fb12331cc9720757db8a7f8bd21
Mario Grabova <mgrabova@gmail.com>
2017-07-12 19:04:35 +0200

    This is my first commit

---
+++ src/main/java/com/beta/firstlogin/FirstloginApplication.java
@@ -0,0 +1,12 @@
+package com.beta.firstlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstloginApplication.class, args);
+	}
+}
---
+++ src/test/java/com/beta/firstlogin/FirstloginApplicationTests.java
@@ -0,0 +1,16 @@
+package com.beta.firstlogin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstloginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
