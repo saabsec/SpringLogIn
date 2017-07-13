153d08415dee828d70deb3db7534d5479ac255e2
Fatjona Avdulaj <fatjona.avdulaj@gmail.com>
2017-07-13 22:16:10 +0200

    Realationship between backend and frontend

--- src/main/resources/static/app/controllers/singin.controller.js
+++ src/main/resources/static/app/controllers/singin.controller.js
@@ -2,8 +2,13 @@
 
   'use strict';
   angular.module('app', [])
-   .controller('SinginCtrl', function($scope) {
-     $scope.greeting = {id: '', content: 'Hello Jona good job this time!'}
+   .controller('SinginCtrl', function($scope, $http) {
+     $scope.greeting = {id: '', content: 'Hello Jona good job this time!'};
+     
+     $http.get('/api/v1/resource').then(function(response) {
+       console.log(response);
+       $scope.greeting = response.data;
+     });
    });
   
 })();
